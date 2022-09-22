4d741a7b4b79d8d2b6499cf2a661ffe82f25a592
bpeddapudi <82531209+bpeddapudi@users.noreply.github.com>
2022-09-22 15:42:27 -0700

    Comments and edit delete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,7 +17,30 @@ function Cars() {
   }
 
   const handleSubmit = (event) => {
+    /**
+     * Gather all the form data to state variable carFormData
+     * When the form is submitted POST the data to Backend using fetch post
+     * https://googlechrome.github.io/samples/fetch-api/fetch-post.html
+     */
     event.preventDefault();
+  }
+
+  const handleDelete = () => {
+    /**
+     * When clicked on a delete button, get the id of the car's delete button clicked
+     * Then use javascript fetch to send DELETE request to NodeJS
+     * https://openjavascript.info/2022/01/03/using-fetch-to-make-get-post-put-and-delete-requests/
+     */
+  }
+
+/** 🥳🥳🥳🥳🥳🥳 DOUBLE BONUS POINTS 🥳🥳🥳🥳🥳🥳 */
+  const handleEdit = () => {
+    /**
+     * When clicked on a edit button figure out a way to edit the car data.
+     * Once edited send the updated data to NodeJS.
+     * Then use javascript fetch to send DELETE request to NodeJS
+     * https://openjavascript.info/2022/01/03/using-fetch-to-make-get-post-put-and-delete-requests/
+     */
   }
  
   return (
@@ -50,6 +73,8 @@ function Cars() {
             <th>ID</th>
             <th>Car Make</th>
             <th>Car Model</th>
+            <th>Edit</th>
+            <th>Delete</th>
           </tr>
         </thead>
         <tbody>
@@ -62,31 +87,43 @@ function Cars() {
             <td>Alfreds Futterkiste</td>
             <td>Maria Anders</td>
             <td>Germany</td>
+            <td>✎</td>
+            <td>🗑</td>        
           </tr>
           <tr>
             <td>Centro comercial Moctezuma</td>
             <td>Francisco Chang</td>
             <td>Mexico</td>
+            <td>✎</td>
+            <td>🗑</td>
           </tr>
           <tr>
             <td>Ernst Handel</td>
             <td>Roland Mendel</td>
             <td>Austria</td>
+            <td>✎</td>
+            <td>🗑</td>
           </tr>
           <tr>
             <td>Island Trading</td>
             <td>Helen Bennett</td>
             <td>UK</td>
+            <td>✎</td>
+            <td>🗑</td>
           </tr>
           <tr>
             <td>Laughing Bacchus Winecellars</td>
             <td>Yoshi Tannamuri</td>
             <td>Canada</td>
+            <td>✎</td>
+            <td>🗑</td>
           </tr>
           <tr>
             <td>Magazzini Alimentari Riuniti</td>
             <td>Giovanni Rovelli</td>
             <td>Italy</td>
+            <td>✎</td>
+            <td>🗑</td>
           </tr>
         </tbody>
       </table>
