05bc79a784e1602b11675335c0aa73b3f30fbeee
timiaraines <72035201+timiaraines@users.noreply.github.com>
2022-10-27 19:59:50 -0500

    frontend

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,15 @@
 
 /** Reference code: https://github.com/bpeddapudi/nodejs-basics-routes/blob/master/server.js 
  * import express */
+ const express = require('express');
+ const app = express();
+ const cors = require('cors');
+ const bodyParser = require('body-parser')
+ app.use(cors())
+ app.use(bodyParser.urlencoded({
+    extended:true
+}));
+app.use(bodyParser.json());
 
 let carsMockData = [
     {
@@ -42,7 +51,9 @@ let carsMockData = [
 
 /** Create GET API. API shoudl return  const carsMockData*/
 
-
+app.get('/cars', (req, res) => {
+    res.json(carsMockData);
+});
 
 
 
@@ -50,7 +61,26 @@ let carsMockData = [
  *      Check if car with id exists. If Yes return 500. With message 'Car already exists'
  *      If there is no car with the id, add the new car to  carsMockData and return carsMockData as response */
 
+ app.post("/cars",(req,res)=>{
+    let car = req.body
+    let id = parseInt(car.id)
 
+    let exist = false
+    carsMockData.forEach((element)=>{
+        if( element.id === id){
+            res.sendStatus(500)
+            console.log("Car already exists")
+            exist = true
+        }
+    })
+    if (!exist){
+        carsMockData.push(car)
+        res.sendStatus(200,()=>{
+            console.log("Adding new car")
+        })
+    }
+   
+})
 
 
 
@@ -58,10 +88,46 @@ let carsMockData = [
  *  Check if car with id exists. If No return 500 with error 'No car with given id exist'. 
  *  If there is car with the requested id, update that car's data in 'carsMockData' and return 'carsMockData' */
 
+ app.put("/put",(req,res)=>{
+    let car = req.body
 
+    carsMockData.map((element)=>{
+        if (element.id === car.id){
+            element.id = car.id
+            element.name = car.name
+            element.releaseYear = car.releaseYear
+            element.color = car.color
+
+            res.sendStatus(200)
+        }
+    })
+    res.sendStatus(500)
+    console.log("No car with give id exists")
+})
 
 
 /** Create Delete API. 
  *  Check if car with id exists. If No return 500. With message 'No car with give id exists'
  *  If there is car with the requested id. Delete that car from 'carsMockData' and return 'carsMockData'
 */
+app.delete('/cars/:id',(req,res)=>{
+    
+    const {id} = req.params
+    let carid = parseInt(id)
+
+    let updated = []
+
+    carsMockData.forEach((element)=>{
+
+        if (element.id !== carid){
+            updated.push(element)
+        }
+    })
+
+    carsMockData = updated
+    res.sendStatus(200)
+})
+
+app.listen(8000,()=>{
+    console.log("connected")
+})
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,8 +13,21 @@ function Cars() {
   const carFormInitialData = {
     id: 0,
     name: ''
+    brand: '',
+    releaseYear: '',
+    color: ''
   }
   const [carFormData, setCarFormData] = useState(carFormInitialData);
+  const [carList, setCarList] = useState([])
+  const [toDelete, setDeleted] = useState('')
+
+
+  useEffect(()=>{
+    fetch('http://localhost:3000/cars')
+    .then(response=>response.json()).then(data=>setCarList(data))
+  },
+  [carFormData, carList])
+
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
@@ -30,15 +43,30 @@ function Cars() {
      * When the form is submitted POST the data to Backend using fetch post
      * https://googlechrome.github.io/samples/fetch-api/fetch-post.html
      */
-    event.preventDefault();
-  }
+     event.preventDefault();
+     fetch('http://localhost:3000/cars',
+     {
+       method: 'POST',
+       headers: {'Content-Type':'application/json'},
+       body: JSON.stringify(carFormData)
+     }
+     )
+     setCarFormData(carFormInitialData)
+   }
 
-  const handleDelete = () => {
+  const handleDelete = (id) => {
     /**
      * When clicked on a delete button, get the id of the car's delete button clicked
      * Then use javascript fetch to send DELETE request to NodeJS
      * https://openjavascript.info/2022/01/03/using-fetch-to-make-get-post-put-and-delete-requests/
      */
+     fetch(`http://localhost:8000/cars/${id}`,
+     {
+       method: 'DELETE',
+       headers: {'Content-Type':'application/json'}
+     }
+     )
+     setDeleted("")
   }
 
 /** 🥳🥳🥳🥳🥳🥳 DOUBLE BONUS POINTS 🥳🥳🥳🥳🥳🥳 */
@@ -58,7 +86,8 @@ function Cars() {
            * TODO: Update the form fields with inputs for 
            *    ID, Brand, Name, ReleaseYear and Color
            * Make required changes to  const carFormInitialData
-           * */}  
+           * */} 
+        
         <label>
           ID:
           <input name='id' type="text" value={carFormData.id} onChange={handleInputChange} />
@@ -66,6 +95,18 @@ function Cars() {
         <label>
           Name:
           <input name='name' type="text" value={carFormData.name} onChange={handleInputChange} />
+        </label>
+        <label>
+          Brand:
+          <input name='brand' type="text" value={carFormData.brand} onChange={handleInputChange} />
+        </label>
+        <label>
+          Release Year:
+          <input name='releaseYear' type="text" value={carFormData.releaseYear} onChange={handleInputChange} />
+        </label>
+        <label>
+          Color:
+          <input name='color' type="text" value={carFormData.color} onChange={handleInputChange} />
         </label>
         <input type="submit" value="Submit" />
       </form>
@@ -90,49 +131,29 @@ function Cars() {
            * TODO: Replace this code with Data from Node JS GET api data
            * React documentation: https://reactjs.org/docs/lists-and-keys.html
            * How to get data from API: https://www.w3schools.com/jsref/api_fetch.asp
-           * */}          
-          <tr>
-            <td>Alfreds Futterkiste</td>
-            <td>Maria Anders</td>
-            <td>Germany</td>
+           * */}
+           {
+            
+           }
+           {
+          carList.map(element=>{
+            return(
+              <tr key={element.id}>
+
+            <td>{element.id}</td>
+            <td>{element.brand}</td>
+            <td>{element.name}</td>
             <td>✎</td>
-            <td>🗑</td>        
+            <td><button
+            onClick={()=>{
+              console.log(element.id)
+              handleDelete(element.id)
+            }}
+            >🗑</button></td>        
           </tr>
-          <tr>
-            <td>Centro comercial Moctezuma</td>
-            <td>Francisco Chang</td>
-            <td>Mexico</td>
-            <td>✎</td>
-            <td>🗑</td>
-          </tr>
-          <tr>
-            <td>Ernst Handel</td>
-            <td>Roland Mendel</td>
-            <td>Austria</td>
-            <td>✎</td>
-            <td>🗑</td>
-          </tr>
-          <tr>
-            <td>Island Trading</td>
-            <td>Helen Bennett</td>
-            <td>UK</td>
-            <td>✎</td>
-            <td>🗑</td>
-          </tr>
-          <tr>
-            <td>Laughing Bacchus Winecellars</td>
-            <td>Yoshi Tannamuri</td>
-            <td>Canada</td>
-            <td>✎</td>
-            <td>🗑</td>
-          </tr>
-          <tr>
-            <td>Magazzini Alimentari Riuniti</td>
-            <td>Giovanni Rovelli</td>
-            <td>Italy</td>
-            <td>✎</td>
-            <td>🗑</td>
-          </tr>
+            )
+          })
+        }       
         </tbody>
       </table>
     </div>
